e778954bd9022a5da99e5e6e3feaa4c3a2ff8add
Kieran Savory <96089380+kieransavory@users.noreply.github.com>
2023-06-24 18:28:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 function scrollCheck(){
 
-
     let currentPos = window.scrollY;
     const heroSection = document.getElementById('heroSection');
-    const heroPos = heroSection.offsetTop
+    const heroPos = heroSection.offsetTop;
 
     const aboutSection = document.getElementById('aboutSection')
     const aboutPos = aboutSection.offsetTop
 
     const tattooSection = document.getElementById('tattooSection');
+    const tattooContainer = document.getElementById('tattooContainer')
     const tattooPos = tattooSection.offsetTop
+
+    const artSection = document.getElementById('artSection')
+    const artText = document.getElementById('artText')
+    const artParallaxPos = document.getElementById('artParallax').offsetTop;
+    const artContainer = document.getElementById('artContainer')
+    const artPos = artSection.offsetTop;
+
+    document.body.addEventListener('load', () => {
+        currentPos = 0
+    })
+
+
+
+    //check if current position is less than or equal to position of hero element and set navbar style accordingly.
 
     if (currentPos <= heroPos) {
         document.getElementById('homenav').style.opacity = "1"
     } else document.getElementById('homenav').style.opacity = "0.5"
+
+    //check if current position is greater than or equal to position of about AND less than tattoos element and set navbar style accordingly.
 
     if (currentPos >= aboutPos && currentPos < tattooPos) {
         document.getElementById('aboutnav').style.opacity = "1";
@@ -23,16 +39,30 @@ function scrollCheck(){
         document.getElementById('aboutnav').style.opacity = "0.5";
     }
 
-    if (currentPos === tattooPos) {
-        document.getElementById('tattoonav').style.opacity = "1";
-    } else document.getElementById('tattoonav').style.opacity = "0.5";
+    //Check current position and fade About text accordingly
 
+    if (currentPos >= aboutPos) {
+        document.getElementById('aboutText').style.opacity = "1"
+    } else document.getElementById('aboutText').style.opacity = "0"
 
+    //Check current position and fade tattoo images accordingly
 
+    if (currentPos >= tattooPos) {
+        tattooContainer.style.opacity = "1"
+    } else tattooContainer.style.opacity = "0"
 
-    console.log(
-       currentPos, tattooPos
-    );
+    //Check current position and fade art text accordingly
+
+    if (currentPos >= artPos) {
+        artText.style.opacity = "1"
+    } else artText.style.opacity = "0"
+
+    //Check current position and fade art images accordingly
+
+    if (currentPos >= artParallaxPos) {
+        artContainer.style.opacity = "1"
+    } else artContainer.style.opacity = "0"
+
 }
 
 function navClickHandler() {
@@ -72,3 +102,31 @@ function tattooScroll() {
     document.getElementById('navbarbutton').classList.toggle('active');
     document.getElementById('buttonspan').classList.toggle('active');
 }
+
+function artScroll() {
+    const artSectionPos = document.getElementById('artSection').offsetTop;
+    window.scrollTo(0,artSectionPos);
+    document.getElementById('navbar').classList.toggle('active');
+    document.getElementById('navbarbutton').classList.toggle('active');
+    document.getElementById('buttonspan').classList.toggle('active');
+}
+
+function faqScroll() {
+    const faqPos = document.getElementById('faq').offsetTop;
+    window.scrollTo(0,faqPos)
+    document.getElementById('navbar').classList.toggle('active');
+    document.getElementById('navbarbutton').classList.toggle('active');
+    document.getElementById('buttonspan').classList.toggle('active');
+}
+
+
+
+
+
+
+  
+
+
+
+ 
+
